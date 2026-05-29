63a2a0d9976d51fbef6cc75302d9802185913597
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-05-29 18:29:21 +0000

    2026-05-29 | Add solution | Java Quiz | 05_Pattern Programming | Number square pattern

---
+++ 02_Java Quiz Assessment/05_Pattern Programming/Module/Number square pattern/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        // TODO: Write code to generate square pattern
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }  
+                                      
+    }
+}
