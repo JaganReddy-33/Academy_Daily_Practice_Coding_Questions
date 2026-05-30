2f97fbf8e3b50b30849d2d72102410fccdba656b
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-05-30 11:46:10 +0000

    2026-05-30 | Add solution | Java Quiz | 05_Pattern Programming | Triangle Pattern 1

--- 02_Java Quiz Assessment/05_Pattern Programming/Module/Triangle Pattern 1/Solution.java
+++ 02_Java Quiz Assessment/05_Pattern Programming/Module/Triangle Pattern 1/Solution.java
@@ -8,7 +8,7 @@ public class Main {
 		int n = scan.nextInt();
 
 		for(int i=1; i<=n; i++){
-            for(int j=1; j<=i; j++){
+            for(int j=1; j<=n; j++){
                 System.out.print(j+" ");
             }
             System.out.println();
