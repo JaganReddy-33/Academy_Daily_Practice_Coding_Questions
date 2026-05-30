56503023b18130ef4c3ac26d2a6a99353e4e55d0
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-05-30 06:54:09 +0000

    2026-05-30 | Add solution | Java Quiz | 05_Pattern Programming | Square Pattern 2

---
+++ 02_Java Quiz Assessment/05_Pattern Programming/Module/Square Pattern 2/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+
+		for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(i*j < 10){
+                    System.out.print('0');
+                }
+                System.out.print(i*j+" ");
+            }
+            System.out.println();
+        }
+
+	}
+}
