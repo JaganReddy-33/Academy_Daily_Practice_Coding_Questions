b2534b32240026ff266c4c5bfe16127681b9de08
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-05-30 06:30:30 +0000

    2026-05-30 | Add solution | Java Quiz | 05_Pattern Programming | Generate a Square Pattern

--- 02_Java Quiz Assessment/05_Pattern Programming/Module/Generate a Square Pattern/Solution.java
+++ 02_Java Quiz Assessment/05_Pattern Programming/Module/Generate a Square Pattern/Solution.java
@@ -1,18 +1,15 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    static void squarePattern(int n)
-    {
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                System.out.print("*");
-            }
-            System.out.println();
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            System.out.print(j+" ");
         }
+        System.out.println();
     }
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        squarePattern(n);
-    }
+  }
 }
