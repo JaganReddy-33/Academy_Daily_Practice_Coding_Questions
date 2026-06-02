02174307bc1f3e9d14095cb2cdbfd3833ff264e5
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-02 05:23:31 +0000

    2026-06-02 | Add solution | Java Quiz | 05_Pattern Programming | Generate Hollow Triangle

---
+++ 02_Java Quiz Assessment/05_Pattern Programming/Module/Generate Hollow Triangle/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=i; k++){
+                if(k==1 || k==i || i==n){
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+	}
+}
