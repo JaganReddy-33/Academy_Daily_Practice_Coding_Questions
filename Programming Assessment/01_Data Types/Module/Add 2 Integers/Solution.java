392aed529517279f3e61076a28e6d18ba877277e
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 17:29:06 +0000

    Add solution: 01_Data Types > Module > Add 2 Integers

---
+++ Programming Assessment/01_Data Types/Module/Add 2 Integers/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    // Write your code here
+    int m = scanner.nextInt();
+    int n = scanner.nextInt();
+
+    int sum = m + n;
+    System.out.print(sum);
+  }
+}
