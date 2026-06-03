b05590d9b5880ed448737bd7b23b72bde17fbdb7
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 14:03:38 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 02_Data Types | Input Double Value

---
+++ 03_Java Coding Scenarios/02_Data Types/Module/Input Double Value/Solution.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Your code here
+        double n = scanner.nextDouble();
+        System.out.print(n);
+    }
+}
