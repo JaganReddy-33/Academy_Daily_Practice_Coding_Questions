9004ef65ba49a026a257fbbea90f05397ef90998
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 16:24:54 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 03_Methods | Add Two Numbers Using a Method

---
+++ 03_Java Coding Scenarios/03_Methods/Module/Add Two Numbers Using a Method/Solution.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+       
+        int a = 5; 
+        int b = 10;
+
+        int result = addNumbers(a, b);
+        System.out.println(result);
+    }
+
+    public static int addNumbers(int x, int y) {
+       int z =  x + y;
+       return z;
+    }
+}
