ee6d50e51936b407e66d5b37e63a2649277bec8f
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 13:50:59 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 01_Main Method | Print Hello World

---
+++ 03_Java Coding Scenarios/01_Main Method/Module/Print Hello World/Solution.java
@@ -0,0 +1,10 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        // Write the code to print "Hello, World!" to the console
+        System.out.print("Hello, World!");
+
+	
+    }
+}
