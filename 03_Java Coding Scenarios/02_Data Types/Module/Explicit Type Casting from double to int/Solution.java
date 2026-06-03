d0d72ef625da70e1de7283b811e5b736ae2b24e9
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 14:12:13 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 02_Data Types | Explicit Type Casting from double to int

---
+++ 03_Java Coding Scenarios/02_Data Types/Module/Explicit Type Casting from double to int/Solution.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        double a = 5.75; // You can change this value for testing
+
+        // Perform explicit type casting from double to int
+        int b = (int)a;
+
+        // Print the original double and casted integer values
+        System.out.println("Original double: " + a);
+        System.out.println("Casted integer: " + b);
+    }
+}
