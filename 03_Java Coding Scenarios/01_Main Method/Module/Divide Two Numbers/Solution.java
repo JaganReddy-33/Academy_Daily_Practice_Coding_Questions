dd05159bb3cb054225e032cb9920f02c9dd1170c
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 13:55:46 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 01_Main Method | Divide Two Numbers

---
+++ 03_Java Coding Scenarios/01_Main Method/Module/Divide Two Numbers/Solution.java
@@ -0,0 +1,15 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        int num1 = 20;
+        int num2 = 4;
+        // Declare and initialize variables 'num1' and 'num2'
+        
+        int c = num1 / num2;
+
+        System.out.print(c);
+        // Write the logic to divide 'num1' by 'num2' and print the result
+
+    }
+}
