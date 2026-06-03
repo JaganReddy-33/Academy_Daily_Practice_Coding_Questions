5d8670e31007183c7bb1d554e3a7a77a5e6c6e87
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 14:21:44 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 03_Methods | Print a Greeting Message Using a Method

---
+++ 03_Java Coding Scenarios/03_Methods/Module/Print a Greeting Message Using a Method/Solution.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        // Call the greeting method
+        printGreeting();
+    }
+
+    // Define the method to print the greeting message
+    public static void printGreeting() {
+        System.out.print("Hello, welcome to the program!");
+    }
+}
