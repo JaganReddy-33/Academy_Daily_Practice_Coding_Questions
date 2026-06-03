355af90f4dafe643055ead6bee8fb4240e0a8467
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 14:10:57 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 02_Data Types | Implicit Type Casting from int to double

---
+++ 03_Java Coding Scenarios/02_Data Types/Module/Implicit Type Casting from int to double/Solution.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 5; 
+
+        // Perform implicit type casting from int to double
+        double b = a;
+
+        // Print the original integer and casted double values
+        System.out.println("Original integer: " + a);
+        System.out.println("Casted double: " + b);
+    }
+}
