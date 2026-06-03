fd9310e38212d6828dd49fd8f4af141f0c317169
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 14:16:07 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 02_Data Types | Mixed Type Casting from float to int and then to byte

---
+++ 03_Java Coding Scenarios/02_Data Types/Module/Mixed Type Casting from float to int and then to byte/Solution.java
@@ -0,0 +1,16 @@
+class Main{
+    public static void main(String[] args) {
+        float a = 123.45f; // You can change this value for testing
+
+        // Perform explicit type casting from float to int
+        int b = (int)a;
+
+        // Perform type casting from int to byte
+        byte c =(byte) b;
+
+        // Print the original float, the casted integer, and the casted byte
+        System.out.println("Original float: " + a);
+        System.out.println("Casted integer: " + b);
+        System.out.println("Casted byte: " + c);
+    }
+}
