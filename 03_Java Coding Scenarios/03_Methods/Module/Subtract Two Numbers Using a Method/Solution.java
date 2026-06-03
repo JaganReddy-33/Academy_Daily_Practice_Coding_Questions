052fa3910587a690c21ea1d696557cda2b639dd6
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 16:29:30 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 03_Methods | Subtract Two Numbers Using a Method

---
+++ 03_Java Coding Scenarios/03_Methods/Module/Subtract Two Numbers Using a Method/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+
+    // Define the method to subtract two integers and return the difference
+    public static int subtraction(int x, int y){
+        return x - y;
+    }
+
+
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from the user
+        Scanner scanner = new Scanner(System.in);
+
+        // Read two integers from the user, each on a separate line
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int result = subtraction(a, b);
+        System.out.println(result);
+    }
+
+  
+}
