46b4bbaca3ba4c32589cc6e320ab8685c53886f0
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 14:05:20 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 02_Data Types | Input Long Value

---
+++ 03_Java Coding Scenarios/02_Data Types/Module/Input Long Value/Solution.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Your code here
+        long n = scanner.nextLong();
+        System.out.print(n);
+    }
+}
