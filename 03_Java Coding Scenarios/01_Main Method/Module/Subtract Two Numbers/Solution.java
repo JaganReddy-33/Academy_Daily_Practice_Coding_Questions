36e0035e70ecc87bff459d3ca130e2c9fc431b2d
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 13:53:32 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 01_Main Method | Subtract Two Numbers

---
+++ 03_Java Coding Scenarios/01_Main Method/Module/Subtract Two Numbers/Solution.java
@@ -0,0 +1,15 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        int a = 15;
+        int b = 10;
+        // Declare and initialize the variable 'b'
+        int c = a - b;
+        
+        System.out.print(c);
+        // Write the logic to subtract 'b' from 'a' and print the result
+
+    }
+}
+
