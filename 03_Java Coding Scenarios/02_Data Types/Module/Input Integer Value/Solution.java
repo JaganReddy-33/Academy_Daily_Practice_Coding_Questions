a5cea7b113320ef1467a7a06a496eec6a5b55c4c
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 13:59:09 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 02_Data Types | Input Integer Value

---
+++ 03_Java Coding Scenarios/02_Data Types/Module/Input Integer Value/Solution.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Your code here
+        int n = scanner.nextInt();
+        System.out.print(n);
+    }
+}
