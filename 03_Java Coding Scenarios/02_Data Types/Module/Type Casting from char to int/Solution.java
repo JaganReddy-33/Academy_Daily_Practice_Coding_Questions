0995c5cb169ef6aa5b8a05284077f304643c6711
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 14:12:59 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 02_Data Types | Type Casting from char to int

---
+++ 03_Java Coding Scenarios/02_Data Types/Module/Type Casting from char to int/Solution.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(String[] args) {
+        char a = 'A'; // You can change this value for testing
+
+        // Perform type casting from char to int
+        int b = a;
+
+        // Print the original character and the corresponding integer value
+        System.out.println("Original character: " + a);
+        System.out.println("Corresponding integer: " + b);
+    }
+}
