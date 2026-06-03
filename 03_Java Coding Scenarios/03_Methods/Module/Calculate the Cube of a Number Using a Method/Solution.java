893f17259e3f0ca81f6acd0cd0fa79da1f7a132b
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 16:44:24 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 03_Methods | Calculate the Cube of a Number Using a Method

---
+++ 03_Java Coding Scenarios/03_Methods/Module/Calculate the Cube of a Number Using a Method/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static int cubeOfNumber(int n){
+        return n*n*n;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int res = cubeOfNumber(n);
+        System.out.print(res);
+
+    }
+}
