4ce3ee2a252af7de958fa9eb9c425a8cc2944288
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 16:42:37 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 03_Methods | Calculate the Square of a Number Using a Method

---
+++ 03_Java Coding Scenarios/03_Methods/Module/Calculate the Square of a Number Using a Method/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static int squareNumber(int n){
+        return n*n;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int result = squareNumber(n);
+        System.out.print(result);
+      
+    }
+}
