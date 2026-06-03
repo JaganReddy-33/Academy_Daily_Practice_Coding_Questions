89eb5d8b5a1564ce62ed0d94eb88725862347751
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 13:54:39 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 01_Main Method | Multiply Two Numbers

---
+++ 03_Java Coding Scenarios/01_Main Method/Module/Multiply Two Numbers/Solution.java
@@ -0,0 +1,15 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        int x = 8;
+        int y = 2;
+        // Declare and initialize the variable 'y'
+
+        int c = x * y;
+
+        System.out.print(c);
+        // Write the logic to multiply 'x' and 'y' and print the result
+
+    }
+}
