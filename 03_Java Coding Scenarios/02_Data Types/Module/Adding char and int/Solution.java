b24315f34a1e8b70586d7e8ad02383fcdd5af8f4
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 14:07:44 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 02_Data Types | Adding char and int

---
+++ 03_Java Coding Scenarios/02_Data Types/Module/Adding char and int/Solution.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void main(String[] args) {
+        char a = 'A'; // You can change this value for testing
+        int b = 5;    // You can change this value for testing
+
+        // Add char to int
+        int result = a + b;
+        // int result = ...;
+
+        // Print the result of adding char and int
+        System.out.println("Result: " + result);
+    }
+}
