f8d05f06b9bcf70114603d8becd98bae01cd0dad
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-03 16:37:42 +0000

    2026-06-03 | Add solution | Java Coding Scenarios | 03_Methods | Find the Maximum of Two Numbers Using a Method

---
+++ 03_Java Coding Scenarios/03_Methods/Module/Find the Maximum of Two Numbers Using a Method/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+    public static int maxNumber(int a, int b){
+        if(a > b){
+            return a;
+        } else {
+            return b;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b =scanner.nextInt();
+
+        int result = maxNumber(a, b);
+        System.out.print(result);
+
+    }
+
+   
+}
