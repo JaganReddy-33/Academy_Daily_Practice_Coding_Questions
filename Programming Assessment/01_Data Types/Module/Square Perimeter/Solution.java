e165e0b43f3a01799fc34c7b215ef425800fd9b5
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-04 17:30:17 +0000

    Add solution: 01_Data Types > Module > Square Perimeter

---
+++ Programming Assessment/01_Data Types/Module/Square Perimeter/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    // Write your code here
+    int n = scanner.nextInt();
+    
+    int res = 4 
+  }
+}
