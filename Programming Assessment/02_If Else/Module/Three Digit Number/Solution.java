250f8796694d84e1acabd38bae087e8771e594cc
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-05 17:33:09 +0000

    Add solution: 02_If Else > Module > Three Digit Number

---
+++ Programming Assessment/02_If Else/Module/Three Digit Number/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        String res = (n>99 && n<1000) ? "Yes" : "No";
+        System.out.print(res);
+        
+    }
+}
