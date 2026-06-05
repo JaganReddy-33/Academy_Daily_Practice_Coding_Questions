121937fa721fe17df31e28783f97bb18eb658658
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-05 17:35:47 +0000

    Add solution: If Else > Module > Positive or Negative Number

---
+++ Programming Assessment/If Else/Module/Positive or Negative Number/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+       if(n>0){
+        System.out.print("Positive");
+       } else if(n<0){
+        System.out.print("Negative");
+       } else {
+        System.out.print("Zero");
+       }
+
+        
+    }
+}
