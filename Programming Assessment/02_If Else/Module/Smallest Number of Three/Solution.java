1b195c274b12c7f521486a41e8c6b755c743b982
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-05 17:36:51 +0000

    Add solution: 02_If Else > Module > Smallest Number of Three

---
+++ Programming Assessment/02_If Else/Module/Smallest Number of Three/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int l = scanner.nextInt();
+
+        if(n<m && n<l){
+            System.out.print(n);
+        } else if(m<l && m<n){
+            System.out.print(m);
+        } else {
+            System.out.print(l);
+        }
+        
+    }
+}
