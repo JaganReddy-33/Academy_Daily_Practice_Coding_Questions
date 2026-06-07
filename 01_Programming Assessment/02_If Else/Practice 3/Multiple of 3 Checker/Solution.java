144e5c9b85f3d902d41d11deb0f0423251becfda
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-07 17:38:57 +0000

    2026-06-07 | Add solution | Programming | 02_If Else | Multiple of 3 Checker

---
+++ 01_Programming Assessment/02_If Else/Practice 3/Multiple of 3 Checker/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        // Your code goes here
+        if(num%3 == 0 && num%6 != 0){
+            System.out.print("Yes");
+        } else {
+            System.out.print("No");
+        }
+    }
+}
