183f054f550f16e0b430c5919c8493b5c4f4eaa8
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-08 18:45:47 +0000

    2026-06-08 | Add solution | Java Coding Scenarios | 04_Arrays | Store and Print the Ages of Students in Two Schools

---
+++ 03_Java Coding Scenarios/04_Arrays/Module/Store and Print the Ages of Students in Two Schools/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        int m=scanner.nextInt();
+        int p=scanner.nextInt();
+
+        int[][][] arr = new int[n][m][p];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                for(int k=0; k<p; k++){
+                    arr[i][j][k] = scanner.nextInt();
+                }
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                for(int k=0; k<p; k++){
+                    System.out.print(arr[i][j][k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
