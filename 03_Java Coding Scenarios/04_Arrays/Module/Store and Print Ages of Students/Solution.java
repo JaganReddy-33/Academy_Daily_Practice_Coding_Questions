5be6b9f6d2bba5ffc2ed43cf1f8a355b413d3037
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-08 17:52:57 +0000

    2026-06-08 | Add solution | Java Coding Scenarios | 04_Arrays | Store and Print Ages of Students

---
+++ 03_Java Coding Scenarios/04_Arrays/Module/Store and Print Ages of Students/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] ages = new int[N];
+
+        for(int i=0; i< N; i++){
+            ages[i] = scanner.nextInt();        
+        }
+
+        System.out.print("Ages are: ");
+        for(int i=0; i<N; i++){
+            System.out.print(ages[i]+" ");
+            if(i<N-1){
+                System.out.print(" ");
+            }
+        }
+    }
+}
