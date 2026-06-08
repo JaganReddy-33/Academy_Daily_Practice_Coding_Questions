7b0762657e5447b03fba74f987b95dd309980233
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-08 17:58:19 +0000

    2026-06-08 | Add solution | Java Coding Scenarios | 04_Arrays | Store and Print Phone Numbers as Long Values

---
+++ 03_Java Coding Scenarios/04_Arrays/Module/Store and Print Phone Numbers as Long Values/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        long[] arr = new long[n];
+        for(int i=0; i<n; i++){
+            arr[i] = scanner.nextLong();
+        }
+
+        System.out.print("Phone numbers are: ");
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
