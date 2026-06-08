8e4130fa638061dfd7ddf6ab322fc0f8e4f04b5e
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-08 18:12:47 +0000

    2026-06-08 | Add solution | Java Coding Scenarios | 04_Arrays | Store and Print Boolean Values Using an Array

---
+++ 03_Java Coding Scenarios/04_Arrays/Module/Store and Print Boolean Values Using an Array/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n =scanner.nextInt();
+        boolean[] arr = new boolean[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = scanner.nextBoolean();
+        }
+
+        for(int i=0; i<n; i++){
+            System.out.println("Value at index "+i+" is: "+arr[i]);
+        }
+    }
+}
