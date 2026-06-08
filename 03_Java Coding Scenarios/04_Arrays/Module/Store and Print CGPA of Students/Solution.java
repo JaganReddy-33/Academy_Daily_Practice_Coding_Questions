5d52317a74b4c0ff91df366514190f511fbc98f2
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-08 18:04:22 +0000

    2026-06-08 | Add solution | Java Coding Scenarios | 04_Arrays | Store and Print CGPA of Students

---
+++ 03_Java Coding Scenarios/04_Arrays/Module/Store and Print CGPA of Students/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n =scanner.nextInt();
+        float[] arr = new float[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = scanner.nextFloat();
+        }
+
+        for(int i=0; i<n; i++){
+            System.out.println("CGPA of student "+(i+1)+" is: "+arr[i]);
+        }
+    }
+}
