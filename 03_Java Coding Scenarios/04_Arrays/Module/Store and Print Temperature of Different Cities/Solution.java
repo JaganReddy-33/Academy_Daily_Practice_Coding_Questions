5418887b5f27601cb60635135f7dea33bdc6f4b6
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-08 18:06:56 +0000

    2026-06-08 | Add solution | Java Coding Scenarios | 04_Arrays | Store and Print Temperature of Different Cities

---
+++ 03_Java Coding Scenarios/04_Arrays/Module/Store and Print Temperature of Different Cities/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n =scanner.nextInt();
+        double[] arr = new double[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = scanner.nextDouble();
+        }
+
+        for(int i=0; i<n; i++){
+            System.out.println("Temperature of city "+(i+1)+" is: "+ arr[i]);
+        }
+    }
+}
