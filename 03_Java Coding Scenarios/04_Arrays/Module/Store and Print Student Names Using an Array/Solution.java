0a1449a0da228cdf98b342528131f196a151bf6c
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-08 18:10:10 +0000

    2026-06-08 | Add solution | Java Coding Scenarios | 04_Arrays | Store and Print Student Names Using an Array

---
+++ 03_Java Coding Scenarios/04_Arrays/Module/Store and Print Student Names Using an Array/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String[] arr = new String[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = scanner.next();
+        }
+
+        for(int i=0; i<n; i++){
+            System.out.println("Student at index "+i+" is: "+arr[i]);
+        }
+    }
+}
