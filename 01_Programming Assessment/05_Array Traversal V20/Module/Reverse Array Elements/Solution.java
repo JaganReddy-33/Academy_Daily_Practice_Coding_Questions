b471284ce791934c94ad68c98b210a0ab64a5de6
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-09 18:27:53 +0000

    2026-06-09 | Add solution | Programming | 05_Array Traversal V20 | Reverse Array Elements

---
+++ 01_Programming Assessment/05_Array Traversal V20/Module/Reverse Array Elements/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void reverseArray(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        reverseArray(arr);
+
+        for(int i=0; i<N; i++){
+            
+        }
+    }
+}
