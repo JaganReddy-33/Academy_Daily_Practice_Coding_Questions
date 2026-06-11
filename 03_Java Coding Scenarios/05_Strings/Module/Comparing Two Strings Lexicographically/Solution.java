36766fc5afe7dc63e6f882eb4545534fafad5d8e
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-11 14:05:26 +0000

    2026-06-11 | Add solution | Java Coding Scenarios | 05_Strings | Comparing Two Strings Lexicographically

---
+++ 03_Java Coding Scenarios/05_Strings/Module/Comparing Two Strings Lexicographically/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str1 = scanner.nextLine();
+        String str2 = scanner.nextLine();
+
+        int n =str1.compareTo(str2);
+
+        if(n>0){
+            System.out.print("First string is greater.");
+        } else if(n<0){
+            System.out.print("Second string is greater.");
+        } else {
+            System.out.print("Both strings are equal.");
+        }
+    }
+}
