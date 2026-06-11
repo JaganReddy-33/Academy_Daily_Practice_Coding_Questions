52c539ee9ff97da1afb12476549efc5ea7b304d3
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-11 13:57:23 +0000

    2026-06-11 | Add solution | Java Coding Scenarios | 05_Strings | Comparing Two Strings for Equality

---
+++ 03_Java Coding Scenarios/05_Strings/Module/Comparing Two Strings for Equality/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String str1 = scanner.nextLine();
+        String str2 = scanner.nextLine();
+
+        if(str1.equals(str2)){
+            System.out.print("true");
+        } else {
+            System.out.print("false");
+        }
+    }
+}
