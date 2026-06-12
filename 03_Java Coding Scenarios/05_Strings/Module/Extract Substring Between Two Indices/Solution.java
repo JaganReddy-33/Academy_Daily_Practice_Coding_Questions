ac347c232a37b69940e3c460673d695e37a2aa1e
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-12 17:51:56 +0000

    2026-06-12 | Add solution | Java Coding Scenarios | 05_Strings | Extract Substring Between Two Indices

---
+++ 03_Java Coding Scenarios/05_Strings/Module/Extract Substring Between Two Indices/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      String str = scanner.nextLine();
+      int idx1 = scanner.nextInt();
+      int idx2 = scanner.nextInt();
+
+      System.out.print(str.substring(idx1, idx2));
+  }
+}
