f0967647be363c921296bde1e929dd50614904f5
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-12 18:07:00 +0000

    2026-06-12 | Add solution | Java Coding Scenarios | 05_Strings | Replace Characters and Convert to Lowercase

---
+++ 03_Java Coding Scenarios/05_Strings/Module/Replace Characters and Convert to Lowercase/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      String str = scanner.nextLine();
+      String ch1 = scanner.next();
+      String ch2 = scanner.next();
+
+      System.out.print(str.replace(ch1, ch2).toLowerCase());
+    }
+}
