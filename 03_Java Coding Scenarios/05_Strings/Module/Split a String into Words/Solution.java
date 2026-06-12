1e60bbfd53abf70fb9bc4eb35a37913bee5781f5
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-12 18:03:19 +0000

    2026-06-12 | Add solution | Java Coding Scenarios | 05_Strings | Split a String into Words

---
+++ 03_Java Coding Scenarios/05_Strings/Module/Split a String into Words/Solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+    String str  =scanner.nextLine();
+
+    String[] words = str.split("\\s+");
+
+    for(String word : words){
+        System.out.println(word);
+    }
+    }
+}
