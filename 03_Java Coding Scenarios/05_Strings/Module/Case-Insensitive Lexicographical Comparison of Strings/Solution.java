2895e3d25e140b988bce1aca0bbba183d6d92816
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-12 17:14:38 +0000

    2026-06-12 | Add solution | Java Coding Scenarios | 05_Strings | Case-Insensitive Lexicographical Comparison of Strings

---
+++ 03_Java Coding Scenarios/05_Strings/Module/Case-Insensitive Lexicographical Comparison of Strings/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.nextLine();
+        String str2 = scanner.nextLine();
+        int n = str1.compareToIgnoreCase(str2);
+        if(n > 0){
+            System.out.print("The first string is greater.");
+        } else if(n < 0){
+            System.out.print("The second string is greater.");
+        } else {
+            System.out.print("Both strings are equal.");
+        }
+    }
+}
