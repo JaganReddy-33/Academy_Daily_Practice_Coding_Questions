212629b80e5b32d413641e78b905d81f2aa05727
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-13 18:42:23 +0000

    2026-06-13 | Add solution | Programming | 06_Array Pairs | Even Pairs

---
+++ 01_Programming Assessment/06_Array Pairs/Module/Even Pairs/Solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+
+    public static void evenPairs(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]%2 == 0 && arr[j]%2 == 0){
+                    System.out.println(arr[i]+" "+arr[j]);
+                }
+            }
+        }
+    }
+
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    int[] arr = new int[n];
+    for(int i=0; i<n; i++){
+        arr[i]=scanner.nextInt();
+    }
+
+    evenPairs(arr);
+  }
+}
