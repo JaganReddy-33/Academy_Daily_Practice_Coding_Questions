10df7861bedf2d44234feab86359f2c7d2a28c39
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-14 18:43:36 +0000

    Add solution: 06_Array Pairs > Module > Counting Pairs

---
+++ Programming Assessment/06_Array Pairs/Module/Counting Pairs/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void countingPairs(int[] arr){
+        int count = 0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                count++;
+            }
+        }
+        System.out.print(count);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] arr = new int[N];
+        for(int i=0; i<arr.length; i++){
+            arr[i]= scanner.nextInt();
+        }
+
+        countingPairs(arr);
+    }
+}
