46b94d37df3a5e4c810d2a34c0c671b8e1756e93
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-14 18:43:51 +0000

    Add solution: Array Pairs > Module > Pairs with Sum as Prime Number

---
+++ Programming Assessment/Array Pairs/Module/Pairs with Sum as Prime Number/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void pairWithSumAsPrimeNum(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                int sum = arr[i]+arr[j];
+                if(isPrime(sum)){
+                    System.out.println(arr[i]+" "+arr[j]);
+                }
+            }
+        }
+    }
+
+    public static boolean isPrime(int num){
+        if(num <= 1 ) return false;
+        for(int i=2; i*i<=num; i++){
+            if(num%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=scanner.nextInt();
+        }
+
+        pairWithSumAsPrimeNum(arr);
+        
+    }
+}
