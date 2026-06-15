97db22ec0f79c2ff90c36686b6e080c4013900c2
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-15 17:28:46 +0000

    2026-06-15 | Add solution | Java Coding Scenarios | 05_Strings | Print a Single Character

---
+++ 03_Java Coding Scenarios/05_Strings/Module/Print a Single Character/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                String str = scanner.next();
+
+                char ch = str.charAt(0);
+
+                System.out.println(ch);
+
+    }
+}
