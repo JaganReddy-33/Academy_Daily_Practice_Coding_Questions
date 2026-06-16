ee468265e6041698ec0989ebf480115b95c8e60d
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-16 05:07:08 +0000

    2026-06-16 | Add solution | Java Coding Scenarios | 06_Method Overloading | Method Overloading with a Calculator Class

---
+++ 03_Java Coding Scenarios/06_Method Overloading/Module/Method Overloading with a Calculator Class/Solution.java
@@ -0,0 +1,44 @@
+class Calculator {
+    // Method to add two integers
+   public int add(int a, int b){
+    return a+b;
+   }
+
+    // Method to add three double numbers
+   public double add(double a, double b, double c){
+    return a+b+c;
+   }
+
+    // Method to add two float numbers
+   public float add(float a, float b){
+    return a+b;
+   }
+
+    // Method to add one integer and one double
+   public double add(int a, double b){
+    return a+b;
+   }
+}
+
+class Main {
+    public static void main(String[] args) {
+        // Create an object of the Calculator class
+        Calculator calculator = new Calculator();
+
+        // Call the add method for two integers
+        int intSum = calculator.add(5, 10);
+        System.out.println("Sum of two integers: " + intSum);
+
+        // Call the add method for three doubles
+        double doubleSum = calculator.add(3.5, 4.5, 5.5);
+        System.out.println("Sum of three doubles: " + doubleSum);
+
+        // Call the add method for two floats
+        float floatSum = calculator.add(2.5f, 3.5f);
+        System.out.println("Sum of two floats: " + floatSum);
+
+        // Call the add method for an integer and a double
+        double mixedSum = calculator.add(4, 7.5);
+        System.out.println("Sum of an integer and a double: " + mixedSum);
+    }
+}
