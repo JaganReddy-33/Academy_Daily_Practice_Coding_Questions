0e6624f263f5680fbcd59b6a8f8a3635c9f9c06e
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-16 05:33:15 +0000

    2026-06-16 | Add solution | Java Coding Scenarios | 06_Method Overloading | Method Overloading with a Calculator Class for Division

---
+++ 03_Java Coding Scenarios/06_Method Overloading/Module/Method Overloading with a Calculator Class for Division/Solution.java
@@ -0,0 +1,41 @@
+class Calculator {
+    // Method to divide two integers
+    public int divide(int a, int b){
+        return a/b;
+    }
+
+    // Method to divide three double numbers
+    public double divide(double a, double b, double c){
+        return a/b/c;
+    }
+
+    // Method to divide two float numbers
+  public float divide(float a, float b){
+    return a/b;
+  }
+
+    // Method to divide an integer by a double
+    public double divide(int a, double b){
+        return a/b;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        // Create an object of the Calculator class
+      Calculator calc=new Calculator();
+
+        // Call the divide method for two integers
+      System.out.println("Quotient of two integers: "+calc.divide(20, 5));
+
+        // Call the divide method for three doubles
+       System.out.println("Quotient of three doubles: "+calc.divide(45.0, 3.0, 5.0));
+
+        // Call the divide method for two floats
+       System.out.println("Quotient of two floats: "+calc.divide(9.0f, 3.0f));
+
+        // Call the divide method for an integer and a double
+        System.out.println("Quotient of an integer and a double: "+calc.divide(30, 2.5));
+       
+    }
+}
