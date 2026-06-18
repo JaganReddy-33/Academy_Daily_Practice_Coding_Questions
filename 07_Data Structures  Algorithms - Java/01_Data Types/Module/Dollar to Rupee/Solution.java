3470a4641a5b73e60789f1c72e885a3410cd7819
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-18 11:49:44 +0000

    2026-06-18 | Add solution | Data Structures & Algorithms - Java | 01_Data Types | Dollar to Rupee

---
+++ 07_Data Structures  Algorithms - Java/01_Data Types/Module/Dollar to Rupee/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n =scanner.nextInt();
+    double dollar = 82.73;
+
+    System.out.pritf("%.4f\n", n*dollar);
+
+
+  }
+}
