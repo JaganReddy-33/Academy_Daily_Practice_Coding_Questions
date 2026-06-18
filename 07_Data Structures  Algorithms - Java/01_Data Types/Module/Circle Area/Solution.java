f4fb3b347e1c17e5a4560808462778d5674bafd4
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-18 11:50:23 +0000

    2026-06-18 | Add solution | Data Structures & Algorithms - Java | 01_Data Types | Circle Area

---
+++ 07_Data Structures  Algorithms - Java/01_Data Types/Module/Circle Area/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int r = scanner.nextInt();
+    double pi = 3.142;
+
+    System.out.printf("%.4f\n", pi*r*r);
+  }
+}
