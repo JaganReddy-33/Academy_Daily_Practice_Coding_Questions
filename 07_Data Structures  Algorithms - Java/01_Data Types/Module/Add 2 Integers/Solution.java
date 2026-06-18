f92b15d5d8ffd6cb33f0cd2a62f3afb1e47c66a7
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-18 05:30:19 +0000

    2026-06-18 | Add solution | Data Structures & Algorithms - Java | 01_Data Types | Add 2 Integers

---
+++ 07_Data Structures  Algorithms - Java/01_Data Types/Module/Add 2 Integers/Solution.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int m = scanner.nextInt();
+    int n =scanner.nextInt();
+    System.out.print(m+n);
+  }
+}
