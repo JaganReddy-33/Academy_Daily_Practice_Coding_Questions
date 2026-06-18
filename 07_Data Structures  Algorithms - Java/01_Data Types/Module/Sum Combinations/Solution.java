3c7e752cd83591472a0fcca9a2961ce25d73c004
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-18 11:49:31 +0000

    2026-06-18 | Add solution | Data Structures & Algorithms - Java | 01_Data Types | Sum Combinations

---
+++ 07_Data Structures  Algorithms - Java/01_Data Types/Module/Sum Combinations/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+    int c =scanner.nextInt();
+    int s1 = a+b;
+    int s2 = a+c;
+    int s3 = b+c;
+    System.out.println(s1);
+    System.ou
+  }
+}
