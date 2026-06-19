9792298bc0815969617b51c6267f176681cf4af4
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-19 11:52:52 +0000

    2026-06-19 | Add solution | Data Structures & Algorithms - Java | 02_If Else | Two Digit Number

---
+++ 07_Data Structures  Algorithms - Java/02_If Else/Module/Two Digit Number/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        if(n>=10 && n<=99){
+            System.out.print("Yes");
+        } else {
+            System.out.print("No");
+        }
+        
+    }
+}
