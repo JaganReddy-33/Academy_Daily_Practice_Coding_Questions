0043ea9099067ccf3c15cbb7b0c06c4658436dac
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-19 02:24:06 +0000

    2026-06-19 | Add solution | Data Structures & Algorithms - Java | 01_Data Types | Product of Three

---
+++ 07_Data Structures  Algorithms - Java/01_Data Types/Module/Product of Three/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    long m=scanner.nextInt();
+    long n=scanner.nextInt();
+    long k=scanner.nextInt();
+    System.out.print(m*n*k);
+  }
+}
