b4b2a2bd97f6e9440366fbaa9ecb06e585218b9a
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-19 11:51:49 +0000

    2026-06-19 | Add solution | Data Structures & Algorithms - Java | 02_If Else | Multiple of 5

---
+++ 07_Data Structures  Algorithms - Java/02_If Else/Module/Multiple of 5/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        if(n%5 == 0){
+            System.out.print("Yes");
+        } else {
+            System.out.print("No");
+        }
+        
+    }
+}
