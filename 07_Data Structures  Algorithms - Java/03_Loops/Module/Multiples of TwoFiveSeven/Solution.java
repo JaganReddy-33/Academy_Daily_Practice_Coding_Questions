0cde9721acfd28489a1ce5703d0d17af94e302aa
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-19 11:59:03 +0000

    2026-06-19 | Add solution | Data Structures & Algorithms - Java | 03_Loops | Multiples of TwoFiveSeven

---
+++ 07_Data Structures  Algorithms - Java/03_Loops/Module/Multiples of TwoFiveSeven/Solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    // Write your code here
+    for(int i=1; i<=n; i++){
+        if(i%2==0 || i%5==0 || i%7==0){
+            System.out.print(i+" ");
+        }
+    }
+  }
+}
