f3ee1ebaab9dcc7e14a319fc847bdc807c3bb34b
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-19 11:53:52 +0000

    2026-06-19 | Add solution | Data Structures & Algorithms - Java | 02_If Else | Number Ends with Zero

---
+++ 07_Data Structures  Algorithms - Java/02_If Else/Module/Number Ends with Zero/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if(n%10==0){
+            System.out.print("Yes");
+        } else {
+            System.out.print("No");
+        }
+      
+    }
+}
