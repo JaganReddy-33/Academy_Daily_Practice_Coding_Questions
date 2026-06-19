e3cbb4fa6bb0c389300010ab64839b62f0a76cbf
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-19 11:58:33 +0000

    2026-06-19 | Add solution | Data Structures & Algorithms - Java | 03_Loops | Multiples of Two

---
+++ 07_Data Structures  Algorithms - Java/03_Loops/Module/Multiples of Two/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    // Write your code here
+    for(int i=2; i<=n; i++){
+        System.out.print((i%2 == 0) ? i+" " : "");
+    }
+  }
+}
