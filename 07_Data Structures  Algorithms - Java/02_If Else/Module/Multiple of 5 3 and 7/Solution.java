7fbfe77a93c74c8ce588edb4eef6ed7a9104c4a4
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-19 11:52:36 +0000

    2026-06-19 | Add solution | Data Structures & Algorithms - Java | 02_If Else | Multiple of 5 3 and 7

---
+++ 07_Data Structures  Algorithms - Java/02_If Else/Module/Multiple of 5 3 and 7/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if(n%3==0 && n%5==0 && n%7==0){
+            System.out.print("Yes");
+        } else {
+            System.out.print("No");
+        }
+       
+    }
+}
