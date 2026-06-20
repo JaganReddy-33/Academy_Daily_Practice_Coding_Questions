3f7a2a7f32fd969a4d7960b0a1889d0aaef70a5f
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-20 12:00:47 +0000

    2026-06-20 | Add solution | Data Structures & Algorithms - Java | 03_Loops | Prime Numbers Up to N

---
+++ 07_Data Structures  Algorithms - Java/03_Loops/Module/Prime Numbers Up to N/Solution.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void printPrimeNum(int n){
+        for(int i=2; i<=n; i++){
+            if(isPrime(i)){
+                System.out.print(i+" ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num){
+        if(num <= 1) return false;
+
+        for(int i=2; i*i<=num; i++){
+            if(num%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        
+        printPrimeNum(n);
+       
+    }
+
+    
+}
