6561ff7b85f4a7be18d3ce633cb76d56a9c1c77b
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-20 12:00:16 +0000

    2026-06-20 | Add solution | Data Structures & Algorithms - Java | 03_Loops | Count Factors

---
+++ 07_Data Structures  Algorithms - Java/03_Loops/Module/Count Factors/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+  static void countFactors(int n)
+	{
+        int count = 0;
+		for(int i=1; i<=n; i++){
+            if(n%i == 0){
+                count++;
+            }
+        }
+        System.out.print(count);
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		countFactors(n);
+	}
+}
