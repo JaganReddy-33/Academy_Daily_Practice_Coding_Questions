7df02617deb699e1395f7048e5970e4193ef73e4
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-21 12:05:10 +0000

    2026-06-21 | Add solution | Data Structures & Algorithms - Java | 04_Array Traversal - 1 | Print Even Index Array Elements

---
+++ 07_Data Structures  Algorithms - Java/04_Array Traversal - 1/Module/Print Even Index Array Elements/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        for(int i=0; i<size; i++){
+            arr[i]=scanner.nextInt();
+        }
+
+        printEvenIndexArrayElements(arr);
+        
+    }
+
+    public static void printEvenIndexArrayElements(int[] arr) {
+        for(int i=0; i<arr; i++){
+            if(i%2 == 0){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}
