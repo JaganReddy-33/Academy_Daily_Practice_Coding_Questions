7d251b858f817e2e6469f6d4976adf1740381bca
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-25 15:28:09 +0000

    2026-06-25 | Add solution | Java Coding Scenarios | 07_Encapsulation | Employee Details Management

---
+++ 03_Java Coding Scenarios/07_Encapsulation/Module/Employee Details Management/Solution.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+class Employee{
+
+    private int empID;
+    private String name;
+    private String dept;
+    private float salary;
+
+    public void setEmpID(int empID){
+        this.empID = empID;
+    }
+    public int getEmpID(){
+        return empID;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setDept(String dept){
+        this.dept = dept;
+    }
+    public String getDept(){
+        return dept;
+    }
+
+    public void setSalary(float salary){
+        this.salary = salary;
+    }
+    public float getSalary(){
+        return salary;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+       Employee emp = new Employee();
+
+       emp.setEmpID(Integer.parseInt(sc.nextLine()));
+       emp.setName(sc.nextLine());
+       emp.setDept(sc.nextLine());
+       emp.setSalary(Float.parseFloat(sc.nextLine()));
+
+       System.out.println(emp.getEmpID());
+       System.out.println(emp.getName());
+       System.out.println(emp.getDept());
+       System.out.println(emp.getSalary());
+
+    }
+}
+
