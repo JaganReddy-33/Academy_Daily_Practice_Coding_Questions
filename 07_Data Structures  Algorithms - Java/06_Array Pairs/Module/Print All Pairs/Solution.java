e7ce880cadf2f5c80d875c5fe7315a9db20fd67f
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-25 15:12:16 +0000

    2026-06-25 | Add solution | Data Structures & Algorithms - Java | 06_Array Pairs | Print All Pairs

---
+++ 07_Data Structures  Algorithms - Java/06_Array Pairs/Module/Print All Pairs/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void printAllPair(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                System.out.println(arr[i]+" "+arr[j]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=scanner.nextInt();
+        }
+
+        printAllPair(arr);
+        
+    }
+}
