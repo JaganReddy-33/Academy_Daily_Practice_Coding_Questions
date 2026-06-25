7932d096310f5a444394abd6bcec16e440beb09c
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-25 16:25:49 +0000

    2026-06-25 | Add solution | Java Coding Scenarios | 07_Encapsulation | Product Inventory Management

---
+++ 03_Java Coding Scenarios/07_Encapsulation/Module/Product Inventory Management/Solution.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+class Product{
+
+    private int productId;
+    private String name;
+    private int quantity;
+    private float price;
+
+    public void setProductId(int productId){
+        this.productId = productId;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+    public void setPrice(float price){
+        this.price = price;
+    }
+
+    public int getProductID(){
+        return productId;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
+    public float getPrice(){
+        return price;
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        Product p = new Product();
+
+        p.setProductId(Integer.parseInt(sc.nextLine()));
+        p.setName(sc.nextLine());
+        p.setQuantity(sc.nextInt());
+        p.setPrice(sc.nextFloat());
+
+        System.out.println(p.getProductID());
+        System.out.println(p.getName());
+        System.out.println(p.getQuantity());
+        System.out.println(p.getPrice());
+
+
+
+    }
+}
