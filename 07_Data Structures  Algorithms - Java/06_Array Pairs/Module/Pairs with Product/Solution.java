450a8ff4bab644e0cfacf744516d466cc203a109
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-25 15:13:17 +0000

    2026-06-25 | Add solution | Data Structures & Algorithms - Java | 06_Array Pairs | Pairs with Product

---
+++ 07_Data Structures  Algorithms - Java/06_Array Pairs/Module/Pairs with Product/Solution.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void productPairs(int[] arr, int k){
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]*arr[j] == k){
+                    System.out.println(arr[i]+" "+arr[j]);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i]=scanner.nextInt();
+        }
+
+        int k= scanner.nextInt();
+
+        productPairs(arr, k);
+        
+    }
+}
