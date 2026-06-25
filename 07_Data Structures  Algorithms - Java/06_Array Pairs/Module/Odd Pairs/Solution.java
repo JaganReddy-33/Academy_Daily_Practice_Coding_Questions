13e45daa90bc3a4e0d7f404e72dcb0924fd0a38e
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-25 15:13:02 +0000

    2026-06-25 | Add solution | Data Structures & Algorithms - Java | 06_Array Pairs | Odd Pairs

---
+++ 07_Data Structures  Algorithms - Java/06_Array Pairs/Module/Odd Pairs/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void oddPairs(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]%2 != 0 && arr[j]%2 != 0){
+                    System.out.println(arr[i]+" "+arr[j]);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i]=scanner.nextInt();
+        }
+
+        oddPairs(arr);
+        
+    }
+}
