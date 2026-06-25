fbb94d64b09a409406bede36e5825965ef8d909d
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-25 15:14:47 +0000

    2026-06-25 | Add solution | Data Structures & Algorithms - Java | 06_Array Pairs | Pairs with Second Value Larger

---
+++ 07_Data Structures  Algorithms - Java/06_Array Pairs/Module/Pairs with Second Value Larger/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void pairWithSecondValLarge(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[j] > arr[i]){
+                    System.out.println(arr[i]+" "+arr[j]);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=scanner.nextInt();
+        }
+
+        pairWithSecondValLarge(arr);
+       
+    }
+}
