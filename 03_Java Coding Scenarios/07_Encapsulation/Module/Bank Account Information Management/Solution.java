50d5b89a5a5258db5dc31cd02caa23e575f65a3a
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-25 16:51:47 +0000

    2026-06-25 | Add solution | Java Coding Scenarios | 07_Encapsulation | Bank Account Information Management

---
+++ 03_Java Coding Scenarios/07_Encapsulation/Module/Bank Account Information Management/Solution.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+class BankAccount{
+
+    private String accountNumber;
+    private String accountHolder;
+    private float balance;
+    private String accountType;
+
+
+    public void setNumber(String accountNumber){
+        this.accountNumber = accountNumber;
+    }
+    public void setHolder(String accountHolder){
+        this.accountHolder = accountHolder;
+    }
+    public void SetBalance(float balance){
+        this.balance = balance;
+    }
+    public void setType(String accountType){
+        this.accountType = accountType;
+    }
+
+    public String getNumber(){
+        return accountNumber;
+    }
+    public String getHolder(){
+        return accountHolder;
+    }
+    public float getBalance(){
+        return balance;
+    }
+    public String getType(){
+        return accountType;
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BankAccount ba = new BankAccount();
+
+        ba.setNumber(sc.nextLine());
+        ba.setHolder(sc.nextLine());
+        ba.SetBalance(Float.parseFloat(sc.nextLine()));
+        ba.setType(sc.nextLine());
+
+        System.out.println(ba.getNumber());
+        System.out.println(ba.getHolder());
+        System.out.println(ba.getBalance());
+        System.out.println(ba.getType());
+        
+    }
+}
