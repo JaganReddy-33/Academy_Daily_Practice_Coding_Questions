ead1e0b3045cde5b05d5ec2abed52b6cba943ff1
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-30 15:36:27 +0000

    2026-06-30 | Add solution | Data Structures & Algorithms - Java | 08_Multiple Arrays | Repeated Even Elements

---
+++ 07_Data Structures  Algorithms - Java/08_Multiple Arrays/Module/Repeated Even Elements/Solution.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+
+    public static void repeatedEvenEle(int[] arr1, int[] arr2, int n, int m){
+
+        int i=0; 
+        int j=0;
+        while(i<n && j<m){
+            if(arr1[i]<arr2[j]){
+                i++;
+            } else if(arr1[i]>arr2[j]){
+                j++;
+            } else {
+                if(arr1[i]%2 == 0){
+                    System.out.print(arr1[i]+" ");
+                }
+                i++;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr1 = new int[n];
+        for(int i=0; i<n; i++){
+            arr1[i]=scanner.nextInt();
+        }
+
+        int m = scanner.nextInt();
+        int[] arr2 = new int[m];
+        for(int i=0; i<m; i++){
+            arr2[i]=scanner.nextInt();
+        }
+
+        repeatedEvenEle(arr1, arr2, n, m);
+       
+    }
+}
