11258bd1742d55b783423bad50c4ebe6d5edf117
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-06-30 18:00:19 +0000

    2026-06-30 | Add solution | Programming | 03_Loops | Multiples of 7 Till N

---
+++ 01_Programming Assessment/03_Loops/Practice 1/Multiples of 7 Till N/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        if(n<7){
+            System.out.print(-1);
+            return;
+        }
+
+        for(int i=7; i<=n; i+=7){
+            System.out.print(i+" ");
+        }
+    }
+}
