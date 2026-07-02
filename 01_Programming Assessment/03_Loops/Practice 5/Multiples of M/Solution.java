8c2f21aaa523d9e344b66bea4e9c03f5ee698ce9
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-02 18:05:03 +0000

    2026-07-02 | Add solution | Programming | 03_Loops | Multiples of M

---
+++ 01_Programming Assessment/03_Loops/Practice 5/Multiples of M/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Your code here
+        int n1=scanner.nextInt();
+        int n2=scanner.nextInt();
+        int m=scanner.nextInt();
+
+        for(int i=n1; i<=n2; i++){
+            if(i%m == 0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
