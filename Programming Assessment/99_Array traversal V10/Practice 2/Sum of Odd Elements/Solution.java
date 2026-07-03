0e8474027ca8854464db79b5f623e6596015ebca
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-03 18:16:21 +0000

    Add solution: 99_Array traversal V10 > Practice 2 > Sum of Odd Elements

---
+++ Programming Assessment/99_Array traversal V10/Practice 2/Sum of Odd Elements/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void sumOfOddEle(int[] arr, int n){
+        int sum =0;
+        for(int i=0; i<n; i++){
+            if(arr[i]%2 != 0){
+                sum +=arr[i];
+            }
+        }
+        System.out.print(sum);
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=scanner.nextInt();
+        }
+
+        sumOfOddEle(arr, n);
+    }
+}
