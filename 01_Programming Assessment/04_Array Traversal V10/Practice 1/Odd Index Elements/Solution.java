24c4ae0b53a73309fd60dd44f98d50d9b9c7df25
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-03 18:15:50 +0000

    2026-07-03 | Add solution | Programming | 04_Array Traversal V10 | Odd Index Elements

---
+++ 01_Programming Assessment/04_Array Traversal V10/Practice 1/Odd Index Elements/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Your code here
+        int n=scanner.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=scanner.nextInt();
+        }
+
+        if(n <=)
+        for(int i=0; i<n; i++){
+            if(i%2 != 0){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}
