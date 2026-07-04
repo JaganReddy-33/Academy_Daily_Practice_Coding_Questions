08beb4ab49d0098eeec9733e1c59bf36494a6d6d
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-04 18:18:25 +0000

    Add solution: Array traversal V10 > Practice 3 > Odd Index Elements from Middle to End

---
+++ Programming Assessment/Array traversal V10/Practice 3/Odd Index Elements from Middle to End/Solution.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void oddIndexEle(int[] arr, int n){
+        if(n<=1){
+            System.out.print("None");
+            return;
+        }
+
+        for(int i=n/2; i<n; i++){
+            if(i%2 != 0){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=scanner.nextInt();
+        }
+
+        oddIndexEle(arr, n);
+    }
+}
