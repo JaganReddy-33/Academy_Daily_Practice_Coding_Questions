4994c2dbca67da130bb76bcbb8395255561a09b5
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-04 18:17:23 +0000

    2026-07-04 | Add solution | Programming | 04_Array Traversal V10 | Product of Odd Index Elements

---
+++ 01_Programming Assessment/04_Array Traversal V10/Practice 3/Product of Odd Index Elements/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void productOfOddIdx(int[] arr, int n){
+        int prod = 1;
+
+        if(n <= 1){
+            System.out.print(0);
+            return
+        }
+        for(int i=0; i<n; i++){
+            if(i%2 != 0){
+                prod *=arr[i];
+            }
+        }
+        System.out.print(prod);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        int[] arr =new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=scanner.nextInt();
+        }
+
+        productOfOddIdx(arr, n);
+    }
+}
