7e56ef932049eb07a0f2d3a435aa8f587c12246b
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-05 18:22:03 +0000

    2026-07-05 | Add solution | Programming | 04_Array Traversal V10 | Sum of Positive Elements

---
+++ 01_Programming Assessment/04_Array Traversal V10/Practice 6/Sum of Positive Elements/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void sumOfPositiveEle(int[] arr, int n){
+        long sum =0;
+        for(int i=0; i<n; i++){
+            if(arr[i] > 0){
+                sum +=arr[i];
+            }
+        }
+        System.out.print(sum);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=scanner.nextInt();
+        }
+
+        sumOfPositiveEle(arr, n);
+    }
+}
