9fc57b54de72ab730d32ef3ecf4bb86ae529f3d5
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-12 05:43:46 +0000

    2026-07-12 | Add solution | Java Coding Scenarios | 09_Polymorphism | Polymorphism in Java Calculating Salaries

---
+++ 03_Java Coding Scenarios/09_Polymorphism/Module/Polymorphism in Java Calculating Salaries/Solution.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+
+class Employee {
+  public int calculateSalary() {
+    return 0;
+  }
+}
+
+
+class Manager extends Employee {
+  @Override
+  public int calculateSalary() {
+    return 80000;
+  }
+}
+
+
+class Programmer extends Employee {
+  @Override
+  public int calculateSalary() {
+    return 60000;
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    String role = scanner.nextLine().trim();
+
+    Employee employee = null;
+            
+    if (role.equalsIgnoreCase("Manager")) {
+      employee = new Manager();
+      System.out.println("Manager Salary: $" + employee.calculateSalary());
+    } else if (role.equalsIgnoreCase("Programmer")) {
+      employee = new Programmer();
+      System.out.println("Programmer Salary: $" + employee.calculateSalary());
+    }
+  }
+}
