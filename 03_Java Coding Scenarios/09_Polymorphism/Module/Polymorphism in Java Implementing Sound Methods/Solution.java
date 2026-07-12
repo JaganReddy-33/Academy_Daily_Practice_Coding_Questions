2aa628ec021c4a9ffaff6eff04ba2c2293130da6
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-12 05:25:57 +0000

    2026-07-12 | Add solution | Java Coding Scenarios | 09_Polymorphism | Polymorphism in Java Implementing Sound Methods

---
+++ 03_Java Coding Scenarios/09_Polymorphism/Module/Polymorphism in Java Implementing Sound Methods/Solution.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Animal {
+  public void sound(){
+
+  }
+}
+
+class Bird extends Animal{
+  @Override
+  public void sound(){
+    System.out.print("Chirp");
+  }
+} 
+
+class Cat extends Animal {
+  @Override
+  public void sound(){
+    System.out.print("Meow");
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    String type = scanner.nextLine().trim();
+
+    Animal a = null;
+
+    if(type.equalsIgnoreCase("Bird")){
+      a = new Bird();
+    } else if(type.equalsIgnoreCase("Cat")){
+      a = new Cat();
+    } else {
+      System.out.print("Unknown animal type");
+      return;
+    }
+
+    a.sound();
+  }
+}
