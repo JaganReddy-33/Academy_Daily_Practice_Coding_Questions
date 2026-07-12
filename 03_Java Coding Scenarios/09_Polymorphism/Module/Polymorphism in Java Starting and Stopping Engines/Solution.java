621fa92e80f4b79011216227ef892c7e7eb86851
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-12 05:40:27 +0000

    2026-07-12 | Add solution | Java Coding Scenarios | 09_Polymorphism | Polymorphism in Java Starting and Stopping Engines

---
+++ 03_Java Coding Scenarios/09_Polymorphism/Module/Polymorphism in Java Starting and Stopping Engines/Solution.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+
+class Vehicle {
+  public void startEngine() {
+    System.out.println("Vehicle Engine Started.");
+  }
+
+  public void stopEngine() {
+    System.out.println("Vehicle Engine Stopped.");
+  }
+}
+
+class Car extends Vehicle {
+  @Override
+  public void startEngine() {
+    System.out.println("Car Engine Started.");
+  }
+
+  @Override
+  public void stopEngine() {
+    System.out.println("Car Engine Stopped.");
+  }
+}
+
+class Motorcycle extends Vehicle {
+  @Override
+  public void startEngine() {
+    System.out.println("Motorcycle Engine Started.");
+  }
+
+  @Override
+  public void stopEngine() {
+    System.out.println("Motorcycle Engine Stopped.");
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    String vehicleType = scanner.nextLine().trim();
+
+    Vehicle vehicle = null;
+
+    if (vehicleType.equalsIgnoreCase("Car")) {
+      vehicle = new Car();
+    } else if (vehicleType.equalsIgnoreCase("Motorcycle")) {
+      vehicle = new Motorcycle();
+    }
+      
+    vehicle.startEngine();
+    vehicle.stopEngine();
+  }
+}
