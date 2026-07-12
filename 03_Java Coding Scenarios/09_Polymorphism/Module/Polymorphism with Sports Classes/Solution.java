a1317c2cfef094c0dd193fb2bddf81f1f8709d1c
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-12 05:36:12 +0000

    2026-07-12 | Add solution | Java Coding Scenarios | 09_Polymorphism | Polymorphism with Sports Classes

---
+++ 03_Java Coding Scenarios/09_Polymorphism/Module/Polymorphism with Sports Classes/Solution.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Sports{
+  public void play(){
+
+  }
+}
+
+class Football extends Sports {
+  @Override
+  public void play(){
+    System.out.println("Playing football on the field.");
+  }
+}
+
+class Basketball extends Sports {
+  @Override
+  public void play(){
+    System.out.println("Playing basketball on the court.");
+  }
+}
+
+class Rugby extends Sports {
+  @Override
+  public void play(){
+    System.out.println("Playing rugby on the pitch.");
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    String type = scanner.nextLine();
+
+    Sports s = null;
+
+    if(type.equalsIgnoreCase("Football")){
+      s = new Football();
+    } else if(type.equalsIgnoreCase("Basketball")){
+      s = new Basketball();
+    } else if(type.equalsIgnoreCase("Rugby")){
+      s = new Rugby();
+    }
+
+    s.play();
+  }
+}
