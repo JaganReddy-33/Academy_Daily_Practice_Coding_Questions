a9230c09e23ae3060c0c4641ae09bf16b7b6d603
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-13 18:56:18 +0000

    Add solution: Subarrays > Practice 1 > Even Subarrays

---
+++ Programming Assessment/Subarrays/Practice 1/Even Subarrays/Solution.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        
+        boolean found = false;
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += arr[k];
+                }
+                if (sum % 2 == 0) {
+                    found = true;
+                    for (int k = i; k <= j; k++) {
+                        System.out.print(arr[k] + " ");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+        
+        if (!found) {
+            System.out.println("None");
+        }
+    }
+}
