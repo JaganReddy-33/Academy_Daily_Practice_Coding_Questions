01767e00e98be9afd7dac50f22118fcee9c7e36d
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-14 20:38:11 +0000

    Add solution: Arrays - miscellaneous > Module > Rearrange Array with 0 at the End

---
+++ Programming Assessment/Arrays - miscellaneous/Module/Rearrange Array with 0 at the End/Solution.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void rearrangeWithZeroAtEnd(int[] arr, int n){
+        int idx = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i]>0){
+                arr[idx] = arr[i];
+                idx++;
+            }
+        }
+
+        while(idx < n){
+            arr[idx] = 0;
+            idx++;
+        }
+
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=scanner.nextInt();
+        }
+
+        rearrangeWithZeroAtEnd(arr, n);
+    }
+}
