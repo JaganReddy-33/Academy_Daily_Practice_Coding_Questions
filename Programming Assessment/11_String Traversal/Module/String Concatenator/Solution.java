bb78a1a55f4f9dfed7b81a0d57b6ce14a07ef650
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-15 19:01:50 +0000

    Add solution: 11_String Traversal > Module > String Concatenator

---
+++ Programming Assessment/11_String Traversal/Module/String Concatenator/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input1 = scanner.nextLine();
+        String input2 = scanner.nextLine();
+
+        System.out.println(input1 + input2);
+       
+    }
+}
