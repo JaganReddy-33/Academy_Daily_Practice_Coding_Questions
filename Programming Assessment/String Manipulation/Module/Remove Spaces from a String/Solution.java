170ed89e41bcf04e3bdc74c5805456164693beee
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-21 19:22:21 +0000

    Add solution: String Manipulation > Module > Remove Spaces from a String

---
+++ Programming Assessment/String Manipulation/Module/Remove Spaces from a String/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.print(input.trim())
+        System.out.print(input.strip());
+
+      
+    }
+}
