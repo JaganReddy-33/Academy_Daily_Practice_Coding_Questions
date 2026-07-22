6bf8a1c1da358970bfe4aec88ddae92db93228e8
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-22 19:23:24 +0000

    Add solution: String Manipulation > Module > Remove Special Characters

---
+++ Programming Assessment/String Manipulation/Module/Remove Special Characters/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void removeSpeChars(String input){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<input.length(); i++){
+            char ch = input.charAt(i);
+            if(Character.isLetterOrDigit(ch)){
+                sb.append(ch);
+            }else {
+                sb.append(ch);
+            }
+        }
+        System.out.print(sb.toString());
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        removeSpeChars(input);
+       
+    }
+}
