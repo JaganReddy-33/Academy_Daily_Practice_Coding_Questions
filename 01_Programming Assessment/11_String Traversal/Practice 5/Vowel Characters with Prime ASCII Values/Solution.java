5e283122fc8442201ed68f948a1d6075d74f59ce
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-26 19:12:57 +0000

    2026-07-26 | Add solution | Programming | 11_String Traversal | Vowel Characters with Prime ASCII Values

---
+++ 01_Programming Assessment/11_String Traversal/Practice 5/Vowel Characters with Prime ASCII Values/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        StringBuilder sb = new StringBuilder();
+        String vowels = "aeiouAEIOU";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (vowels.indexOf(c) != -1 && isPrime((int) c)) {
+                sb.append(c);
+            }
+        }
+        if (sb.length() == 0) {
+            System.out.print("-1");
+        } else {
+            System.out.print(sb.toString());
+        }
+    }
+}
