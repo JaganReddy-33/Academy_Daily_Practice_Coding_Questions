3721290e70541294c4934ce6c2fa1cb9c213a03d
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-28 13:21:00 +0000

    2026-07-28 | Add solution | Java Coding Scenarios | 10_Agregation and Compostion | Aggregation and Composition in Java Student with Brain Heart Books and Bikes

---
+++ 03_Java Coding Scenarios/10_Agregation and Compostion/Module/Aggregation and Composition in Java Student with Brain Heart Books and Bikes/Solution.java
@@ -0,0 +1,111 @@
+class Heart {
+    private int weight;
+    private int bpm;
+
+    public Heart(int weight, int bpm) {
+        this.weight = weight;
+        this.bpm = bpm;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getBpm() {
+        return bpm;
+    }
+}
+
+class Brain {
+    private int weight;
+    private String colour;
+
+    public Brain(int weight, String colour) {
+        this.weight = weight;
+        this.colour = colour;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+}
+
+class Bike {
+    private String brand;
+    private int mileage;
+
+    public Bike(String brand, int mileage) {
+        this.brand = brand;
+        this.mileage = mileage;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+}
+
+class Book {
+    private String name;
+    private String author;
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
+
+class Student {
+    private Heart heart;
+    private Brain brain;
+
+    public Student() {
+        this.heart = new Heart(289, 72);
+        this.brain = new Brain(1400, "grey");
+    }
+
+    public void displayHeartAndBrain() {
+        System.out.println(heart.getWeight());
+        System.out.println(heart.getBpm());
+        System.out.println(brain.getWeight());
+        System.out.println(brain.getColour());
+    }
+
+    public void hasA(Bike bike) {
+        System.out.println(bike.getBrand());
+        System.out.println(bike.getMileage());
+    }
+
+    public void hasA(Book book) {
+        System.out.println(book.getName());
+        System.out.println(book.getAuthor());
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.displayHeartAndBrain();
+
+        Bike bike = new Bike("Duke", 35);
+        student.hasA(bike);
+
+        Book book = new Book("Java", "JG");
+        student.hasA(book);
+    }
+}
