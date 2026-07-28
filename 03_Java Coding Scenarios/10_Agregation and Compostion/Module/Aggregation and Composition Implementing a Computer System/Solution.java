89afd722d61f2d9b80dbc8c06d989a222af1595e
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-28 13:22:18 +0000

    2026-07-28 | Add solution | Java Coding Scenarios | 10_Agregation and Compostion | Aggregation and Composition Implementing a Computer System

---
+++ 03_Java Coding Scenarios/10_Agregation and Compostion/Module/Aggregation and Composition Implementing a Computer System/Solution.java
@@ -0,0 +1,72 @@
+class Processor {
+    private String brand;
+    private int cores;
+    private double clockSpeed;
+
+    public Processor(String brand, int cores, double clockSpeed) {
+        this.brand = brand;
+        this.cores = cores;
+        this.clockSpeed = clockSpeed;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getCores() {
+        return cores;
+    }
+
+    public double getClockSpeed() {
+        return clockSpeed;
+    }
+}
+
+class Monitor {
+    private String brand;
+    private int size;
+
+    public Monitor(String brand, int size) {
+        this.brand = brand;
+        this.size = size;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
+
+class Computer {
+    private Processor processor;
+
+    public Computer() {
+        this.processor = new Processor("Intel", 4, 2.5);
+    }
+
+    public void displayProcessor() {
+        System.out.println("Processor Brand: " + processor.getBrand());
+        System.out.println("Processor Cores: " + processor.getCores());
+        System.out.println("Processor Clock Speed: " + processor.getClockSpeed() + " GHz");
+    }
+
+    public void hasA(Monitor monitor) {
+        if (monitor != null) {
+            System.out.println("Monitor Brand: " + monitor.getBrand());
+            System.out.println("Monitor Size: " + monitor.getSize() + " inches");
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Computer computer = new Computer();
+        computer.displayProcessor();
+
+        Monitor monitor = new Monitor("Dell", 24);
+        computer.hasA(monitor);
+    }
+}
