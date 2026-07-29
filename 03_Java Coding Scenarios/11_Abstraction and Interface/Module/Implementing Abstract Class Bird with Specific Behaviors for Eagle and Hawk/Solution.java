79e60b1cc98ae02bca351659a5b839131fa2689b
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-29 09:38:06 +0000

    2026-07-29 | Add solution | Java Coding Scenarios | 11_Abstraction and Interface | Implementing Abstract Class Bird with Specific Behaviors for Eagle and Hawk

---
+++ 03_Java Coding Scenarios/11_Abstraction and Interface/Module/Implementing Abstract Class Bird with Specific Behaviors for Eagle and Hawk/Solution.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+// Abstract class Bird
+abstract class Bird {
+    abstract void fly();
+    abstract void makeSound();
+}
+
+// Eagle class extending Bird
+class Eagle extends Bird {
+    @Override
+    public void fly(){
+        System.out.println("Eagle is soaring at great heights.");
+    }
+
+    @Override
+    public void makeSound(){
+        System.out.println("Eagle makes a sharp, piercing sound.");
+    }
+}
+
+// Hawk class extending Bird
+class Hawk extends Bird {
+    @Override
+    public void fly(){
+        System.out.println("Hawk is flying swiftly over the plains.");
+    }
+
+    @Override
+    public void makeSound(){
+        System.out.println("Hawk makes a screeching sound.");
+    }
+}
+
+
+// Main class
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+        if(str.equals("Eagle")){
+            Bird e = new Eagle();
+            e.fly();
+            e.makeSound();
+        } else if(str.equals("Hawk")){
+            Bird h = new Hawk();
+            h.fly();
+            h.makeSound();
+        }
+    }
+}
