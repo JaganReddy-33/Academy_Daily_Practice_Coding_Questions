7c298491a115249701e3a07d0b8d8dab1bf71730
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-30 04:50:54 +0000

    2026-07-30 | Add solution | Java Coding Scenarios | 12_Exception Handling | Handle Multiple Exceptions

---
+++ 03_Java Coding Scenarios/12_Exception Handling/Module/Handle Multiple Exceptions/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    try{
+      int numerator = scanner.nextInt();
+      int denominator = scanner.nextInt();
+      int res = numerator / denominator;
+      System.out.print("Result: "+res);
+    } catch(ArithmeticException e){
+      System.out.print("Division by zero is not allowed");
+    } catch(InputMismatchException e){
+      System.out.print("Invalid input, please enter integers only");
+    }
+  }
+}
