14944914c28722638916c9c84d564ba9c2978e36
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-30 04:56:27 +0000

    2026-07-30 | Add solution | Java Coding Scenarios | 12_Exception Handling | Handle Multiple Exceptions in Array Operations

---
+++ 03_Java Coding Scenarios/12_Exception Handling/Module/Handle Multiple Exceptions in Array Operations/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    try{
+      int n = scanner.nextInt();
+      int[] arr = new int[n];
+      for(int i=0; i<n; i++){
+        arr[i]=scanner.nextInt();
+      }
+      int idx = scanner.nextInt();
+
+      System.out.print(arr[idx]);
+    } catch(NegativeArraySizeException e){
+      System.out.print("Array size cannot be negative");
+    } catch(ArrayIndexOutOfBoundsException e){
+      System.out.print("Invalid index, index out of bounds");
+    } catch(InputMismatchException e){
+      System.out.print("Invalid input, please enter integers only");
+    }
+  }
+}
