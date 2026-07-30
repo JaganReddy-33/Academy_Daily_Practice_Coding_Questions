01ae4ba91268d61ebd02c296659e4b442c8c32fc
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-30 04:45:34 +0000

    2026-07-30 | Add solution | Java Coding Scenarios | 12_Exception Handling | Handle Negative Array Size Exception

---
+++ 03_Java Coding Scenarios/12_Exception Handling/Module/Handle Negative Array Size Exception/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+
+    try{
+      int[] arr = new int[n];
+      System.out.println("Array created with size: "+n);
+    } catch(NegativeArraySizeException e){
+      System.out.println("Array size cannot be negative");
+    }
+  }
+}
