914864b3ed4d834f882c00b0ce7a9586fd861983
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-30 05:24:16 +0000

    2026-07-30 | Add solution | Java Coding Scenarios | 12_Exception Handling | Exception Handling in Division

---
+++ 03_Java Coding Scenarios/12_Exception Handling/Module/Exception Handling in Division/Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            int num = scanner.nextInt();
+            int den = scanner.nextInt();
+            int res = num / den;
+            System.out.println(res);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Cannot divide by zero");
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Please enter valid integers.");
+        } finally {
+            System.out.println("Operation finished");
+        }
+    }
+}
