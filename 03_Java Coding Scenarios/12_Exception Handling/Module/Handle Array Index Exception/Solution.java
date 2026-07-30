124d5c2e737226026e1981cd054dc651c69ccf4a
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-30 04:05:45 +0000

    2026-07-30 | Add solution | Java Coding Scenarios | 12_Exception Handling | Handle Array Index Exception

---
+++ 03_Java Coding Scenarios/12_Exception Handling/Module/Handle Array Index Exception/Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int size = scanner.nextInt();
+    int[] arr = new int[size];
+    for(int i=0; i<size; i++){
+      arr[i] = scanner.nextInt();
+    }
+    int idx = scanner.nextInt();
+
+    try{
+      System.out.print(arr[idx]);
+    } catch(ArrayIndexOutOfBoundsException e){
+      System.out.print("Invalid index entered");
+    }
+  }
+}
