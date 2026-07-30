5883d371f07b9820c88eb5ae8922373b0bfcfad8
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-30 04:08:59 +0000

    2026-07-30 | Add solution | Java Coding Scenarios | 12_Exception Handling | Handle Number Format Exception

---
+++ 03_Java Coding Scenarios/12_Exception Handling/Module/Handle Number Format Exception/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    
+    try{
+      int num = Integer.parseInt(input);
+      System.out.println(num);
+    } catch(NumberFormatException e){
+      System.out.println("Invalid number format");
+    }
+  }
+}
