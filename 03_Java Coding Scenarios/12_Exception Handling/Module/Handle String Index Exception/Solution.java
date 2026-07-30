5fba8aaa38c631ba0cc736900be327c5adc8202d
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-30 04:11:35 +0000

    2026-07-30 | Add solution | Java Coding Scenarios | 12_Exception Handling | Handle String Index Exception

---
+++ 03_Java Coding Scenarios/12_Exception Handling/Module/Handle String Index Exception/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    int index = scanner.nextInt();
+    
+    try{
+      System.out.println(input.charAt(index));
+    } catch(StringIndexOutOfBoundsException e){
+      System.out.println("Invalid index for string");
+    }
+  }
+}
