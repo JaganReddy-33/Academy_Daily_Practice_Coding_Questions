a4c348f378ccecfef6162527e9083c16ac865cf2
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-30 04:02:27 +0000

    2026-07-30 | Add solution | Java Coding Scenarios | 12_Exception Handling | Handle Arithmetic Exception

---
+++ 03_Java Coding Scenarios/12_Exception Handling/Module/Handle Arithmetic Exception/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int numerator = scanner.nextInt();
+    int denominator = scanner.nextInt();
+
+
+    try{
+      int res = numerator / denominator;
+      System.out.print(res);
+    } catch (ArithmeticException e){
+      System.out.println("Cannot divide by zero");
+    }
+  }
+}
