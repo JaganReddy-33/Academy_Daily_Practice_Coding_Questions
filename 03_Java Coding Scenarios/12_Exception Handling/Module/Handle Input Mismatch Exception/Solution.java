fc40e7455e2ed6915f82572eed8572f0151b992c
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-30 04:16:06 +0000

    2026-07-30 | Add solution | Java Coding Scenarios | 12_Exception Handling | Handle Input Mismatch Exception

---
+++ 03_Java Coding Scenarios/12_Exception Handling/Module/Handle Input Mismatch Exception/Solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    try{
+      int n = scanner.nextInt();
+      System.out.println("The entered integer is: "+n);
+    } catch(InputMismatchException e){
+      System.out.println("Invalid input, please enter an integer");
+    }
+  }
+}
