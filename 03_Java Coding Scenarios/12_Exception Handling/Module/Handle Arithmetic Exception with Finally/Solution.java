c8644ae30a37c5a79809b14cdbb2a0ec148bdf97
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-07-30 05:00:40 +0000

    2026-07-30 | Add solution | Java Coding Scenarios | 12_Exception Handling | Handle Arithmetic Exception with Finally

---
+++ 03_Java Coding Scenarios/12_Exception Handling/Module/Handle Arithmetic Exception with Finally/Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try{
+            int numerator = sc.nextInt();
+            int denominator = sc.nextInt();
+
+            int res = numerator / denominator;
+            System.out.println(res);
+        } catch(ArithmeticException e){
+            System.out.println("Cannot divide by zero");
+        } finally {
+            System.out.println("End of division attempt");
+        }
+    }
+}
