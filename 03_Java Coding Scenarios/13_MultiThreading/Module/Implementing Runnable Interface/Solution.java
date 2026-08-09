70c00dd667966a15722e9843ae1144a6484bf21b
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-08-09 10:43:41 +0000

    2026-08-09 | Add solution | Java Coding Scenarios | 13_MultiThreading | Implementing Runnable Interface

---
+++ 03_Java Coding Scenarios/13_MultiThreading/Module/Implementing Runnable Interface/Solution.java
@@ -0,0 +1,23 @@
+
+class Print implements Runnable {
+    @Override
+    public void run(){
+        for(int i=0; i<=2; i++){
+            System.out.println("Running in Runnable!");
+        }
+    }
+}
+
+public class Main  {
+    public static void main(String[] args) {
+       Print p = new Print();
+       Thread t = new Thread(p);
+       t.start();
+
+       try{
+        t.join();
+       } catch(InterruptedException e){
+        e.printStackTrace();
+       }
+    }
+}
