4a74304665884793283a0cdf950f7e54ec4d12bf
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-08-09 10:33:28 +0000

    2026-08-09 | Add solution | Java Coding Scenarios | 13_MultiThreading | Basic Thread Creation

---
+++ 03_Java Coding Scenarios/13_MultiThreading/Module/Basic Thread Creation/Solution.java
@@ -0,0 +1,21 @@
+
+class Print extends Thread {
+    @Override
+    public void run(){
+        for(int i=0; i<5; i++){
+            System.out.println("Hello from Thread!");
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+       Print p= new Print();
+       p.start();
+       try{
+        p.join();
+       } catch(InterruptedException e){
+        e.printStackTrace();
+       }
+    }
+}
