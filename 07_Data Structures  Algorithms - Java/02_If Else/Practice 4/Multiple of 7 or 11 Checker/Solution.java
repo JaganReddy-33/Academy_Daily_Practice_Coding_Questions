f4f53ace98ddfe0ea4e8ad87670bababad6451d1
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-08-16 12:42:06 +0000

    2026-08-16 | Add solution | Data Structures & Algorithms - Java | 02_If Else | Multiple of 7 or 11 Checker

---
+++ 07_Data Structures  Algorithms - Java/02_If Else/Practice 4/Multiple of 7 or 11 Checker/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long num = scanner.nextLong();
+        if (num % 7 == 0 || num % 11 == 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
