009584c1bb9abba7c38987a4581e12bc978d4bd0
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-08-17 12:48:09 +0000

    2026-08-17 | Add solution | Data Structures & Algorithms - Java | 02_If Else | Factor and Even Check

---
+++ 07_Data Structures  Algorithms - Java/02_If Else/Practice 7/Factor and Even Check/Solution.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int M = scanner.nextInt();
+        int N = scanner.nextInt();
+        System.out.println((M != 0 && N % M == 0 && M % 2 == 0) ? "Yes" : "No");
+    }
+}
