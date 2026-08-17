bb743d07b847ec6896be65861e3284604bb9b0d8
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-08-17 12:46:52 +0000

    2026-08-17 | Add solution | Data Structures & Algorithms - Java | 02_If Else | Uppercase Alphabet ASCII Check

---
+++ 07_Data Structures  Algorithms - Java/02_If Else/Practice 7/Uppercase Alphabet ASCII Check/Solution.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println((num >= 65 && num <= 90) ? "Yes" : "No");
+    }
+}
