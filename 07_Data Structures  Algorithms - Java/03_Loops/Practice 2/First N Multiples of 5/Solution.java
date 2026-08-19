15ab05f359b95f415d9e25cd4245ea6da7bf382e
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-08-19 13:39:10 +0000

    2026-08-19 | Add solution | Data Structures & Algorithms - Java | 03_Loops | First N Multiples of 5

---
+++ 07_Data Structures  Algorithms - Java/03_Loops/Practice 2/First N Multiples of 5/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            if (sb.length() > 0) {
+                sb.append(" ");
+            }
+            sb.append(i * 5);
+        }
+        System.out.print(sb);
+    }
+}
