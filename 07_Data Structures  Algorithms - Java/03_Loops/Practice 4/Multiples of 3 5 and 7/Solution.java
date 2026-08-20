17fbdf7723105812ac1ef7392c7b1feed0b0d1a9
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-08-20 14:29:42 +0000

    2026-08-20 | Add solution | Data Structures & Algorithms - Java | 03_Loops | Multiples of 3 5 and 7

---
+++ 07_Data Structures  Algorithms - Java/03_Loops/Practice 4/Multiples of 3 5 and 7/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+
+        int start = Math.min(n1, n2);
+        int end = Math.max(n1, n2);
+
+        for (int i = start; i <= end; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
