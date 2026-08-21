6bcacd7a6c71b4037521763ac44b7ebcc50d72be
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-08-21 14:41:13 +0000

    2026-08-21 | Add solution | Data Structures & Algorithms - Java | 04_Array Traversal - 1 | Print Float Array

---
+++ 07_Data Structures  Algorithms - Java/04_Array Traversal - 1/Practice 1/Print Float Array/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        float[] arr = new float[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextFloat();
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
