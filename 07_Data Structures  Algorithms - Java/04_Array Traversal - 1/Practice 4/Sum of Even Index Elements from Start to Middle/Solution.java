96c43a914173df569e6363f6560cf099a21ce1aa
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-08-22 14:44:56 +0000

    2026-08-22 | Add solution | Data Structures & Algorithms - Java | 04_Array Traversal - 1 | Sum of Even Index Elements from Start to Middle

---
+++ 07_Data Structures  Algorithms - Java/04_Array Traversal - 1/Practice 4/Sum of Even Index Elements from Start to Middle/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+    public static long sumEvenIndexToMiddle(int[] arr) {
+        int mid = arr.length / 2;
+        long sum = 0;
+        for (int i = 0; i <= mid; i += 2) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(sumEvenIndexToMiddle(arr));
+    }
+}
