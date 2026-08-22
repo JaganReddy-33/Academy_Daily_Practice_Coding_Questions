7f4a7315ec7c7b7a3ee6f2fb15f4ae9dc1a4a29e
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-08-22 14:44:41 +0000

    2026-08-22 | Add solution | Data Structures & Algorithms - Java | 04_Array Traversal - 1 | Even Index Elements from Middle to End

---
+++ 07_Data Structures  Algorithms - Java/04_Array Traversal - 1/Practice 4/Even Index Elements from Middle to End/Solution.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Main {
+    public static void printEvenIndexFromMiddleToEnd(int[] arr) {
+        int mid = arr.length / 2;
+        int start = (mid % 2 == 0) ? mid : mid + 1;
+        boolean found = false;
+        for (int i = start; i < arr.length; i += 2) {
+            System.out.print(arr[i]+" ");
+            found = true;
+        }
+        if (!found) {
+            System.out.print("None");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scanner.nextInt();
+            }
+            printEvenIndexFromMiddleToEnd(arr);
+        }
+    }
+}
