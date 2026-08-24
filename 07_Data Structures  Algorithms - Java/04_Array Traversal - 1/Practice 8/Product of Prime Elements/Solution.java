3cf04f71bd468766589ed6364f6f74c4a2c47441
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-08-24 14:50:35 +0000

    2026-08-24 | Add solution | Data Structures & Algorithms - Java | 04_Array Traversal - 1 | Product of Prime Elements

---
+++ 07_Data Structures  Algorithms - Java/04_Array Traversal - 1/Practice 8/Product of Prime Elements/Solution.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        printProductOfPrimes(arr);
+    }
+
+    public static void printProductOfPrimes(int[] arr) {
+        long product = 1;
+        for (int num : arr) {
+            if (isPrime(num)) {
+                product *= num;
+            }
+        }
+        System.out.println(product);
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; (long) i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
