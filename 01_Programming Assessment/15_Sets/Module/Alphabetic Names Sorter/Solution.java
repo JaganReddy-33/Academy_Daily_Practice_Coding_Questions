0d87f8f4bbb5fea93b2ecc044347873412f3d098
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-08-29 19:33:36 +0000

    2026-08-29 | Add solution | Programming | 15_Sets | Alphabetic Names Sorter

---
+++ 01_Programming Assessment/15_Sets/Module/Alphabetic Names Sorter/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
+        // Your code here
+    }
+}
