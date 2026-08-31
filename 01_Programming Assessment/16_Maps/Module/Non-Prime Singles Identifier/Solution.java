1883f588a694f2aa2a671143ab23fc25072984e8
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-08-31 07:05:44 +0000

    2026-08-31 | Add solution | Programming | 16_Maps | Non-Prime Singles Identifier

---
+++ 01_Programming Assessment/16_Maps/Module/Non-Prime Singles Identifier/Solution.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        printNonPrimeSingles(arr);
+    }
+
+    public static void printNonPrimeSingles(int[] arr) {
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1 && !isPrime(entry.getKey())) {
+                System.out.println(entry.getKey());
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
