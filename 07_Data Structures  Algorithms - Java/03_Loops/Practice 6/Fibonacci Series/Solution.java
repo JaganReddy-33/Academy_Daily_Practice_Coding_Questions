64d50eeee9ad57d84a4142e58da94435752021f1
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-01 14:31:46 +0000

    2026-09-01 | Add solution | Data Structures & Algorithms - Java | 03_Loops | Fibonacci Series

---
+++ 07_Data Structures  Algorithms - Java/03_Loops/Practice 6/Fibonacci Series/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+    static void printFibonacci(int n) {
+        int a = 0, b = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        printFibonacci(n);
+    }
+}
