95a76535ad2dfbff0ea667783b0ae13cf4b07e04
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-02 14:34:15 +0000

    2026-09-02 | Add solution | Data Structures & Algorithms - Java | 03_Loops | Odd Multiples of 3

---
+++ 07_Data Structures  Algorithms - Java/03_Loops/Practice 8/Odd Multiples of 3/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        printOddMultiplesOfThree(n1, n2);
+    }
+
+    public static void printOddMultiplesOfThree(int n1, int n2) {
+        int start = Math.min(n1, n2);
+        int end = Math.max(n1, n2);
+        for (int i = start; i <= end; i++) {
+            if (i % 3 == 0 && i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
