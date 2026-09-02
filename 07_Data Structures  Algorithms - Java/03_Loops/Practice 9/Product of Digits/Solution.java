4467460064140ea805d259d4b100daa6788fe733
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-02 14:35:30 +0000

    2026-09-02 | Add solution | Data Structures & Algorithms - Java | 03_Loops | Product of Digits

---
+++ 07_Data Structures  Algorithms - Java/03_Loops/Practice 9/Product of Digits/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        printProductOfDigits(n);
+    }
+
+    public static void printProductOfDigits(int n) {
+        int product = 1;
+        while (n > 0) {
+            int digit = n % 10;
+            product *= digit;
+            n /= 10;
+        }
+        System.out.println(product);
+    }
+}
