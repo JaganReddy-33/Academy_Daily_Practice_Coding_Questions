4859780c7fefbbfff34261df6c17f31d00d14d68
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-03 14:36:44 +0000

    2026-09-03 | Add solution | Data Structures & Algorithms - Java | 03_Loops | Even Digits

---
+++ 07_Data Structures  Algorithms - Java/03_Loops/Practice 10/Even Digits/Solution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String n = scanner.next();
+        printEvenDigits(n);
+    }
+
+    public static void printEvenDigits(String n) {
+        boolean found = false;
+        for (int i = 0; i < n.length(); i++) {
+            char ch = n.charAt(i);
+            int digit = ch - '0';
+            if (digit % 2 == 0) {
+                if (found) {
+                    System.out.print(" ");
+                }
+                System.out.print(digit);
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.print("No even digits");
+        }
+    }
+}
