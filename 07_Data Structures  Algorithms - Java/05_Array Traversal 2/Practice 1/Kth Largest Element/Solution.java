dbe629ddebc2ff91b32e2d22d515acb06b76fabd
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-04 15:01:02 +0000

    2026-09-04 | Add solution | Data Structures & Algorithms - Java | 05_Array Traversal 2 | Kth Largest Element

---
+++ 07_Data Structures  Algorithms - Java/05_Array Traversal 2/Practice 1/Kth Largest Element/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int k = scanner.nextInt();
+        
+        Arrays.sort(arr);
+        System.out.println(arr[n - k]);
+    }
+}
