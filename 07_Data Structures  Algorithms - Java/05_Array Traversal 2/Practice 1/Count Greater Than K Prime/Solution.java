69542dd1587d0d657c70d26560b7637492d81b6b
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-04 15:02:03 +0000

    2026-09-04 | Add solution | Data Structures & Algorithms - Java | 05_Array Traversal 2 | Count Greater Than K Prime

---
+++ 07_Data Structures  Algorithms - Java/05_Array Traversal 2/Practice 1/Count Greater Than K Prime/Solution.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Main {
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int countGreaterThanKPrime(int[] arr, int k) {
+        int count = 0;
+        for (int num : arr) {
+            if (num > k && isPrime(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int k = scanner.nextInt();
+        System.out.println(countGreaterThanKPrime(arr, k));
+    }
+}
