43f8859f35654cc65d1f2f1f8def1a143da96bb0
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-05 15:04:19 +0000

    2026-09-05 | Add solution | Data Structures & Algorithms - Java | 05_Array Traversal 2 | Replace Even Elements with -1

---
+++ 07_Data Structures  Algorithms - Java/05_Array Traversal 2/Practice 3/Replace Even Elements with -1/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        replaceEvenElements(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void replaceEvenElements(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                arr[i] = -1;
+            }
+        }
+    }
+}
