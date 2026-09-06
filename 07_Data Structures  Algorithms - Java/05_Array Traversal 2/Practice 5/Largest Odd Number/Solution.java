2e0d6abeb387868ca632d202913f438d9177af29
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-06 15:06:48 +0000

    2026-09-06 | Add solution | Data Structures & Algorithms - Java | 05_Array Traversal 2 | Largest Odd Number

---
+++ 07_Data Structures  Algorithms - Java/05_Array Traversal 2/Practice 5/Largest Odd Number/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        findLargestOdd(arr);
+    }
+
+    public static void findLargestOdd(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        boolean found = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+                found = true;
+            }
+        }
+        if (found) {
+            System.out.println(max);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
