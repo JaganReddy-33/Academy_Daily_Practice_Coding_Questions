f482bf59c283bd2764be3c7b1078abb0b2f8d9cd
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-06 15:06:03 +0000

    2026-09-06 | Add solution | Data Structures & Algorithms - Java | 05_Array Traversal 2 | Second Smallest Even

---
+++ 07_Data Structures  Algorithms - Java/05_Array Traversal 2/Practice 5/Second Smallest Even/Solution.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        findSecondSmallestEven(arr);
+    }
+
+    public static void findSecondSmallestEven(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                if (arr[i] < min) {
+                    secondMin = min;
+                    min = arr[i];
+                } else if (arr[i] < secondMin && arr[i] != min) {
+                    secondMin = arr[i];
+                }
+            }
+        }
+        if (secondMin != Integer.MAX_VALUE) {
+            System.out.println(secondMin);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
