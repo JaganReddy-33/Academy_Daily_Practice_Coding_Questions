61be5bc92bcbc31067be53bf1665e6c58e0afdfb
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-08 08:23:45 +0000

    2026-09-08 | Add solution | Data Structures & Algorithms - Java | 05_Array Traversal 2 | Occurrence of Largest Element

---
+++ 07_Data Structures  Algorithms - Java/05_Array Traversal 2/Practice 8/Occurrence of Largest Element/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(countLargestOccurrence(arr, n));
+    }
+
+    public static int countLargestOccurrence(int[] arr, int n) {
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == max) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
