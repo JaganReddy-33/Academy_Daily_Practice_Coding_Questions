204aa65be8487f6895a217651f89598c7580969b
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-08 15:10:02 +0000

    2026-09-08 | Add solution | Data Structures & Algorithms - Java | 05_Array Traversal 2 | Second Largest Element

---
+++ 07_Data Structures  Algorithms - Java/05_Array Traversal 2/Practice 8/Second Largest Element/Solution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(findSecondLargest(arr, n));
+    }
+
+    public static int findSecondLargest(int[] arr, int n) {
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                secondMax = max;
+                max = arr[i];
+            } else if (arr[i] > secondMax && arr[i] != max) {
+                secondMax = arr[i];
+            }
+        }
+        return secondMax;
+    }
+}
