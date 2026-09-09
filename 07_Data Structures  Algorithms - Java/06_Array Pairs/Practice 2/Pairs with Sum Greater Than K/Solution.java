405e018cb4cc4b1c65e603324136968d9b44401e
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-09 15:18:33 +0000

    2026-09-09 | Add solution | Data Structures & Algorithms - Java | 06_Array Pairs | Pairs with Sum Greater Than K

---
+++ 07_Data Structures  Algorithms - Java/06_Array Pairs/Practice 2/Pairs with Sum Greater Than K/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int k = scanner.nextInt();
+        printPairsGreaterThanK(arr, k);
+    }
+
+    public static void printPairsGreaterThanK(int[] arr, int k) {
+        boolean found = false;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] > k) {
+                    System.out.println(arr[i] + " " + arr[j]);
+                    found = true;
+                }
+            }
+        }
+        if (!found) {
+            System.out.println("No pairs found");
+        }
+    }
+}
