d9350e9ffc7d83bbdd005fce4ce768c089931ccc
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-10 15:26:48 +0000

    2026-09-10 | Add solution | Data Structures & Algorithms - Java | 07_Sorted Arrays | Ascending Sort

---
+++ 07_Data Structures  Algorithms - Java/07_Sorted Arrays/Practice 1/Ascending Sort/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        sortArray(arr);
+        printArray(arr);
+    }
+
+    public static void sortArray(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
