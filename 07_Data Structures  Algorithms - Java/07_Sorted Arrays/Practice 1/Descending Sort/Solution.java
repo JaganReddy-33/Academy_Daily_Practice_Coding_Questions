30cabaf1c206554f90f0b3fad8c1b0b5f475104f
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-10 15:27:03 +0000

    2026-09-10 | Add solution | Data Structures & Algorithms - Java | 07_Sorted Arrays | Descending Sort

---
+++ 07_Data Structures  Algorithms - Java/07_Sorted Arrays/Practice 1/Descending Sort/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        sortArrayDescending(arr);
+        printArray(arr);
+    }
+
+    public static void sortArrayDescending(int[] arr) {
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
