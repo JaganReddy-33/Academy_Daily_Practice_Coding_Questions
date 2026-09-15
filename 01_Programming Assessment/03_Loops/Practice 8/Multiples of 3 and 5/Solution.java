05ef2b0d6f86ab5cc2ef9104dbca908894a0788e
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-15 17:52:32 +0000

    2026-09-15 | Add solution | Programming | 03_Loops | Multiples of 3 and 5

---
+++ 01_Programming Assessment/03_Loops/Practice 8/Multiples of 3 and 5/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+
+        printMultiplesOfThreeAndFiveDescending(n1, n2);
+    }
+
+    public static void printMultiplesOfThreeAndFiveDescending(int n1, int n2) {
+
+        int start = Math.max(n1, n2);
+        int end = Math.min(n1, n2);
+        for (int i = start; i >= end; i--) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                System.out.print(i + " ");
+                
+            }
+        }
+    }
+}
