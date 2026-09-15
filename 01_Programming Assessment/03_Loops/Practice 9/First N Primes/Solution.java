f873fadafc7a937764f36954476e759befcc0e2f
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-15 17:55:26 +0000

    2026-09-15 | Add solution | Programming | 03_Loops | First N Primes

---
+++ 01_Programming Assessment/03_Loops/Practice 9/First N Primes/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        printFirstNPrimesGreaterThanM(n, m);
+    }
+
+    public static void printFirstNPrimesGreaterThanM(int n, int m) {
+        int count = 0;
+        int num = m + 1;
+        while (count < n) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+                count++;
+            }
+            num++;
+
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
