9319b19ea4337b4c66713c4653d1820871970d4e
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-09-16 16:38:35 +0000

    2026-09-16 | Add solution | Programming | 03_Loops | Odd Digits Count

---
+++ 01_Programming Assessment/03_Loops/Practice 10/Odd Digits Count/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        printOddDigitsCount(n);
+    }
+
+    public static void printOddDigitsCount(int n) {
+        
+        int count = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit % 2 != 0) {
+                count++;
+            }
+            n /= 10;
+        }
+        System.out.println(count);
+    }
+}
